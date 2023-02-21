e8b0d2eae716e9c7f4a749ec06542f19af819d0e
alrshdn <mohammed.alrashdan.yes@gmail.com>
2023-02-21 22:36:17 +0300

    Initialized repository.

---
+++ patch/include.h
@@ -0,0 +1,19 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "cyclelayouts.h"
+#include "rotatestack.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
